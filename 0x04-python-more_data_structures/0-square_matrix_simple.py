0e0b0ab8b6bb863e9ff7d2173c5693a9855a7733
allvo6377 <alvinpilot@gmail.com>
2023-03-15 18:59:09 -0700

    quick fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
-def uniq_add(my_list=[]):
-    uniq_list = set(my_list)
-    num = 0
+def square_matrix_simple(matrix=[]):
+    '''
+        A function that returns the square value of all elements in a
+        multi dimensional list been passed as arguments.
+        Returns a new matrix that is of same size as the size of the
+        matrix passed to it as arguments.
+        You are allowed to use regular loops, map, etc
+    '''
+    new_lst = []
+    if len(matrix) == 0:
+        return new_lst
 
-    for i in uniq_list:
-        num += i
-
-    return (num)
+    new_lst = [[i*i for i in j] for j in matrix]
+    return new_lst
