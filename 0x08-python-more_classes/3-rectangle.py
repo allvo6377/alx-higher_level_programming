80dd0d3660e408643931b1754fd96d609c16e235
allvo6377 <alvinpilot@gmail.com>
2023-04-04 01:45:39 +0300

    fix

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,13 +56,14 @@ class Rectangle:
             return 2 * (self.__width + self.__height)
     
     def __str__(self):
-        """Returns a printable string representation
-        of a Rectangle  with the '#' character."""
-        if self.__height == 0 or self.__width == 0:
+
+        """Returnsa printable string representation of the # character"""
+        if self.width == 0 or self.height == 0:
             return ""
-        rec_str = ""
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rec_str += "#"
-            rec_str += "\n"
-        return rec_str[:-1]
+        else:
+            rect_str = ""
+            for i in range(self.height):
+                rect_str += "#" * self.width
+                if i != self.height - 1:
+                    rect_str += "\n"
+            return rect_str 
