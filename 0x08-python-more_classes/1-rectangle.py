1e2a6e0eaa999ac8617368b09a08232338f5e07b
allvo6377 <alvinpilot@gmail.com>
2023-04-03 20:00:52 +0000

    Task one complete

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/bin/usr/python
+"""A module that defines a clss rectangle"""
+
+
+class Rectangle:
+    """Defines a class rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """Initializes a new rectangle with a width and a height
+
+        Args:
+        width: an integer that represents object width
+        height: an integer that represents object height
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """Gets the width attribute(private)"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Sets the width attribute value"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Gets the height of rectangle."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Sets height of rectangle"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
