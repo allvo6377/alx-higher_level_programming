3121fabc172eaa9dcf493a374d700affc3be7a53
allvo6377 <alvinpilot@gmail.com>
2023-04-04 20:11:11 +0000

    task 2

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python
-"""Defines a matrix division function"""
+#!/usr/bin/python3
+
 
 def matrix_divided(matrix, div):
     """
@@ -19,13 +19,16 @@ def matrix_divided(matrix, div):
         a number (integer or float).
         ZeroDivisionError: If div is equal to 0.
     """
-    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix ):
-        raise TypeError("matrix must be a matrix(list of lists) of integers/floats")
+    if not isinstance(matrix, list) or not all(isinstance(row, list)
+                                               for row in matrix):
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
     if not all(isinstance(x, (int, float)) for row in matrix for x in row):
-        raise TypeError("matrix must be a matrix(lists of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists)"
+                        " of integers/floats")
     row_length = len(matrix[0])
     if not all(len(row) == row_length for row in matrix):
-        raise TypeError("Each row of matrix must have the same size")
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if div == 0:
