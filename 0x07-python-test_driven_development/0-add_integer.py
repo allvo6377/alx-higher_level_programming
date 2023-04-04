7565be28d0174878b1971b34346fc89c0b9175b9
allvo6377 <alvinpilot@gmail.com>
2023-04-04 18:40:02 +0000

    task 0 complete

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Sum of integers"""
+
+def add_integer(a, b=98):
+    """Adds two numbers
+    Args:
+    a - first number input
+    b - second number input
+    """
+    if type (a) not in [int, float] or type (b) not in [int, float]:
+        raise TypeError("a must be an integer or b must be an integer")
+    return int(a) + int(b)
