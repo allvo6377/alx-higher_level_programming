9787fbb4f9bffbadd9b7605c287655d37f851b39
allvo6377 <alvinpilot@gmail.com>
2023-04-04 03:01:09 +0300

    __del__ fix complete

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -72,6 +72,5 @@ class Rectangle:
         return "Rectangle({}, {})".format(self.__width, self.__height)
     
     def __del__(self):
-
         """Prints a goodbye message on delete"""
         print("Bye rectangle...")
