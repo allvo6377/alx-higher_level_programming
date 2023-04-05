a39ec720b165267a4190cf5eed52039704c6bf5c
allvo6377 <alvinpilot@gmail.com>
2023-04-05 09:38:20 +0000

    Convert to executable!

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,30 @@
 #!/usr/bin/python3
-"""Sum of integers"""
+"""
+This is the "0-add_integer" module.
+The 0-add_integer module supplies one function, add_integer(). For example,
+>>> add_integer(10, 12)
+"""
 
 
 def add_integer(a, b=98):
-    """Adds two numbers
-    Args:
-    a - first number input
-    b - second number input
     """
-    if type(a) not in [int, float] or type(b) not in [int, float]:
-        raise TypeError("a must be an integer or b must be an integer")
-    return int(a) + int(b)
+        Adds two integer.
+        Args:
+            a (int|float): An integer or floating point number
+            b (int|float): An integer or floating point number
+        Returns: The sum of `a` and `b`
+    """
+
+    if type(a) is float:
+        a = int(a)
+
+    if type(b) is float:
+        b = int(b)
+
+    if type(a) is not int:
+        raise TypeError("a must be an integer")
+
+    if type(b) is not int:
+        raise TypeError("b must be an integer")
+
+    return (a + b)
