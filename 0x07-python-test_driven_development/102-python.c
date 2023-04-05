451a23ec55c3e98b1d11d8887d25918c083c5a13
allvo6377 <alvinpilot@gmail.com>
2023-04-05 02:21:35 +0000

    Convert to executable!

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,26 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_string(PyObject *p)
+{
+    Py_ssize_t len;
+    wchar_t *str;
+
+    printf("[.] string object info\n");
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        printf("  type: compact ascii\n");
+    else
+        printf("  type: compact unicode object\n");
+
+    len = PyUnicode_GET_LENGTH(p);
+    str = PyUnicode_AsWideCharString(p, &len);
+
+    printf("  length: %ld\n", len);
+    printf("  value: %ls\n", str);
+}
