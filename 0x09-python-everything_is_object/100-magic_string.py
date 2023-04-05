0643745eecb55a885c8d75deaad3f9218f4ff94f
allvo6377 <alvinpilot@gmail.com>
2023-04-05 01:40:43 +0000

    Adv tasks complete!

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    return "BestSchool" * n
+    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
+    return ", ".join(["BestSchool"] * magic_string.counter)
