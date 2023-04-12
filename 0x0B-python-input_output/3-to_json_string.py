86814b0ea061a96c99f203bc98a4aad94dffcbb0
allvo6377 <alvinpilot@gmail.com>
2023-04-13 02:29:53 +0300

    mandatory task 4

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -2,6 +2,7 @@
 """Defines a module that returns json object"""
 
 
+import json
 def to_json_string(my_obj):
     """A function that returns a string representation of an object"""
     return json.dumps(my_obj)
