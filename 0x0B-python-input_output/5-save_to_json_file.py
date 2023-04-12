a768227b32a124a49c3e5bf341622f257ca656a2
allvo6377 <alvinpilot@gmail.com>
2023-04-13 02:56:32 +0300

    Mandatory tasks

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,5 +6,7 @@ import json
 
 
 def save_to_json_file(my_obj, filename):
+    """A function that writes an object to text file using,
+    json representation"""
     with open(filename, mode="w") as f:
         return j.dumps(my_obj, f)
