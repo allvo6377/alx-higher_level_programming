3744d2a945c51779f031487f9a1962a63b1854e1
allvo6377 <alvinpilot@gmail.com>
2023-04-13 04:11:56 +0300

    mandatory task completed

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+"""Returns dictionary descrition of data structure"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary description with simple data structure,
+    for JSON serialization of an object"""
+    return obj.__dict__
