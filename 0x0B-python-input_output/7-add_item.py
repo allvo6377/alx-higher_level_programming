79a52d517ca449a0302fb0daec186bcae3a8fb06
allvo6377 <alvinpilot@gmail.com>
2023-04-13 03:50:23 +0300

    mandatory task complete

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,14 +4,15 @@ and then save them to a file"""
 
 
 import sys
-from os import path
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
 
-filename = "add_item.json"
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-else:
-    my_list = []
-my_list.extend(sys.argv[1:])
-save_to_json_file(my_list, filename)
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
