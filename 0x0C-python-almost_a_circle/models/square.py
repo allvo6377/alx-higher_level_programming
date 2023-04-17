d9571cf7ec3376b587d26165b0343c81ee9bcf52
allvo6377 <alvinpilot@gmail.com>
2023-04-17 12:13:08 +0000

    added methods that assigns attributes

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,14 +18,45 @@ class Square(Rectangle):
                                                  self.width)
 
     @property
-    
     def size(self):
         """Get the size of the square."""
         return self.width
 
     @size.setter
-
-    def size(self,value):
+    def size(self, value):
         """Sets the size of the square"""
         self.width = value
         self.height = value
+
+    def update(self, *args, **kwargs):
+        """Assign an argument to each attribute
+        Args:
+            - id attribute
+            - size attribute
+            - x attribute
+            - y attribute
+        """
+
+        if args is not None and len(args) != 0:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.size = args[1]
+            if len(args) > 2:
+                self.x = args[2]
+            if len(args) > 3:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
