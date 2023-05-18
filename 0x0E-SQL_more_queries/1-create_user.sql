a042018cbcef62b14d5d4726e61abdac8104f64b
allvo6377 <alvinpilot@gmail.com>
2023-05-18 05:06:49 +0300

    task one quick correction

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,7 @@
--- Creates user user_0d_1
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
+-- Creates the user user_0d_1
 CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost';
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
