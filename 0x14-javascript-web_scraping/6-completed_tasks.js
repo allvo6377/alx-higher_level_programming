c4634c688005b38e90282e3c918a795fc4d732ee
allvo6377 <alvinpilot@gmail.com>
2023-07-26 01:56:24 +0300

    Task 6 fixed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
 
-'use strict';
+const request = require('request');
+const url = process.argv[2];
 
-const requestMod = require('request');
-const requestUrl = process.argv[2];
-
-requestMod(requestUrl, function (error, response, body) {
+request(url, function (error, response, body) {
   if (error) {
     console.log(error);
-  } else {
-    const result = JSON.parse(body);
-    const dic = {};
-    for (let index = 0; index < result.length; index++) {
-      if (result[index].completed === true) {
-        if (dic[result[index].userId] === undefined) {
-          dic[result[index].userId] = 1;
-        } else {
-          dic[result[index].userId] += 1;
-        }
+  }
+  const completed = {};
+  for (const task of JSON.parse(body)) {
+    if (task.completed === true) {
+      if (completed[task.userId]) {
+        completed[task.userId]++;
+      } else {
+        completed[task.userId] = 1;
       }
     }
-    console.log(dic);
   }
+  console.log(completed);
 });
