9c127989849f9279f1f5cc25118ed3ee1fea0155
allvo6377 <alvinpilot@gmail.com>
2023-07-26 01:32:54 +0300

    updated fix

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,25 @@
 #!/usr/bin/node
+const request = require('request')
 
-const request = require('request');
-const url = process.argv[2];
+const apiUrl = 'https://jsonplaceholder.typicode.com/todos'
 
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
-      }
+request(apiUrl, (error, response, body) => {
+  const todos = JSON.parse(body)
+
+  const completedByUser = {}
+
+  todos.forEach(todo => {
+    if (todo.completed) {
+      const userId = todo.userId
+      if (completedByUser[userId]) {
+        completedByUser[userId]++
+      } else {
+        completedByUser[userId] = 1
+      } 
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
-});
+  })
+
+  Object.keys(completedByUser).forEach(userId => {
+    console.log(`User ${userId} completed ${completedByUser[userId]} tasks`)
+  })
+})
